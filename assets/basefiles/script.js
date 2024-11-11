620d8ac206602de7f46061ee56eae42b65c0f808
SusKeyyy <166961563+SysKeyyyJS@users.noreply.github.com>
2024-11-11 08:51:40 -0500

    Commit #3 11-11-24 8:51:00AM
    Created modals, and added base details, script.js, style.css and credits added to base site.

---
+++ assets/basefiles/script.js
@@ -0,0 +1,10 @@
+// Function to open the modal when the 'More Info' button is clicked
+function openModal(productId) {
+    document.getElementById(productId).style.display = 'flex';
+  }
+  
+  // Function to close the modal when the 'X' button is clicked
+  function closeModal(productId) {
+    document.getElementById(productId).style.display = 'none';
+  }
+  
